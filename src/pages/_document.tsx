48c08beadb51eeda7919a25764f79fed02f2d25f
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-24 17:22:33 -0300

    feat: add next '_document' file

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+// Document carrega apenas uma vez ao acessar a aplicação
+// -> O App recarrega durante as mudanças de tela
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
