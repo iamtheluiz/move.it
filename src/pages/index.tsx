fe6c58c32cb2dcbbb951b8291e2084cde24d2d0b
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-25 16:52:00 -0300

    feat: add Countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 
 import styles from '../styles/pages/Home.module.css'
 
@@ -12,6 +13,7 @@ export default function Home() {
         <div >
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
