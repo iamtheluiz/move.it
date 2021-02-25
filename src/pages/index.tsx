25459731d95f596a55d0151937d265e8666a4d2d
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-25 16:55:42 -0300

    feat: add favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
@@ -8,6 +10,9 @@ import styles from '../styles/pages/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div >
