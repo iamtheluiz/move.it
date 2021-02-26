ce4f6fb54231cb4e5fca42202eb260652da37439
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-02-26 17:18:48 -0300

    feat: reset Countdown state

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -48,6 +48,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     clearTimeout(countdownTimeout);
     setIsActive(false);
     setTime(0.05 * 60);
+    setHasFinished(false);
   }
 
   return (
