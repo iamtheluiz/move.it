6dbf930bc08873e345d6d2597b78863b146dbd8f
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-03-01 00:30:00 -0300

    feat: PWA config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
+import Head from 'next/head'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Pomodoro with levels!" />
+        <meta name="keywords" content="Pomodoro" />
+        <title>Move.it</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#5965E0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
